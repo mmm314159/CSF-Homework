216d57e92368af9aa93b921cc8937337fb1f5266
GORBRA14 <gorbra14@ccm-2610-b4.evergreen.edu>
2013-10-31 16:21:32 -0700

    Updated quiz 2

---
+++ quiz2.py
@@ -0,0 +1,17 @@
+# From a module called lab2
+# import a variable called n
+# print one of the following things for n
+# greater than 50
+# less than 50
+# equal to 50
+
+
+
+from lab2 import n
+
+if n > 50:
+    print "greater than 50"
+if n < 50:
+    print "less than 50"
+if n == 50:
+    print "equal to 50"
